2f80c9adaaeeca778ea2dadc83b4f701653068b5
eligeske <eligeske@gmail.com>
2014-02-06 21:04:01 -0500

    .

---
+++ models/js.js
@@ -0,0 +1,64 @@
+// promoter logged in home page (after registration has occurred)
+
+var promoter = {
+    firstName: "",
+    lastName: "",
+    email: ""   
+}
+
+// events (add, remove)
+var events = [
+    {
+        id: 1,
+        date: "2014-2-1", /* notice this date has passed, so there is laps attached to the racer */
+        name: "race number 1",
+        description: "First series race cross country",
+        eventType: {
+            id: 1,
+            name: "Mountain Bike"
+        },
+        categorySettings: [
+            { laps: 5, category: { id: 1, name: "Expert" } },
+            { laps: 3, category: { id: 2, name: "Beginner" } }
+        ],
+        registeredRacers: [
+            { 
+                id: 12, firstName: "", lastName: "", bib: "124", category: { id: 1, name: "Expert" }, team: { id: 34, name: "Bubba's Race Team" },
+                laps: [
+                    { time: "12:12:59", place: "5" },
+                    { time: "10:45:32", place: "2" },
+                    { time: "11:03:45", place: "2" },
+                    { time: "10:52:23", place: "3" },
+                    { time: "11:02:42", place: "1" }
+                ] 
+            },
+            { 
+                id: 67, firstName: "", lastName: "", bib: "46",  category: { id: 2, name: "Beginner" }, team: null,
+                laps: [
+                    { time: "10:52:23", place: "4" },
+                    { time: "10:52:23", place: "3" },
+                    { time: "11:02:42", place: "5" }
+                ]  
+            }
+        ]
+    },
+    {
+        id: 2,
+        date: "2014-2-10",
+        name: "race number 2",
+        description: "Second series race cross country",
+        eventType: {
+            id: 1,
+            name: "Mountain Bike"
+        },        
+        categorySettings: [
+            { laps: 5, category: { id: 1, name: "Expert" } },
+            { laps: 3, category: { id: 2, name: "Beginner" } }
+        ],
+        registeredRacers: [
+            { id: 12, firstName: "", lastName: "", bib: "124", category: { id: 1, name: "Expert" }, team: { id: 34, name: "Bubba's Race Team" } },
+            { id: 67, firstName: "", lastName: "", bib: "46", category: { id: 1, name: "Expert" }, team: null },
+            { id: 89, firstName: "", lastName: "", bib: "46", category: { id: 1, name: "Expert" }, team: null }
+        ]
+    }
+]
